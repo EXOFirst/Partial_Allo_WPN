e78b90b00aaaf5fc805ebd4b18a4593cc32dc0db
Ant9615 <rudals901@gmail.com>
2022-05-12 15:47:10 +0900

    EXOfir 0.0.2

--- agents_DDPG.py
+++ agents_DDPG.py
@@ -4,39 +4,32 @@ And this work submitted to KMMS spring conference.
 this simulation result will 
 '''
 
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow import keras 
-from collections import deque
-import numpy as np
-
 
 def rand_agent(env, state):
     ois_1 = state[0]
-    Dis = state[1] 
-    Eus = state[2] 
-    
+    Dis = state[1]
+    Eus = state[2]
+
     alarm = True
     while alarm:
         FUmax = env.FU[1] * 0.9
-        FUmin = env.FU[0] 
-        FImax = env.FI[1] * 0.9 
-        FImin = env.FI[0]
-
-        fus = np.random.rand(env.N) 
-        ois = np.random.rand(env.N)
+        FUmin = env.FU[0] * 0.9
+        FImax = env.FI[1] * 0.9
+        FImin = env.FI[0] * 0.9
 
         fus = np.random.rand(env.N) * (FUmax-FUmin) + FUmin
         fli = np.random.rand(env.N) * (FImax-FImin) + FImin
         ois = np.random.rand(env.N)
 
         tlis = env.local(fli, ois_1, Dis)[1]
-        Eexe ,texe = env.computing(fus, ois_1, Dis)[1]
+        Eexe, texe = env.computing(fus, ois_1, Dis)[
+            0], env.computing(fus, ois_1, Dis)[1]
 
         alarm = bool((tlis < env.ts) | (texe < env.ts) & (Eexe < Eus))
-    
-    action = [fus, ois]
-    return action 
+
+    action = [fus, fli, ois]
+    return action
+
 
 class OUActionNoise:
     def __init__(self, mean, std_deviation, theta=0.15, dt=1e-2, x_initial=None):
@@ -49,8 +42,8 @@ class OUActionNoise:
 
     def __call__(self):
         x = (self.x_prev
-            + self.theta * (self.mean - self.x_prev) * self.dt
-            + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape))
+             + self.theta * (self.mean - self.x_prev) * self.dt
+             + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape))
         # Store x into x_prev
         # Makes next noise dependent on current one
         self.x_prev = x
@@ -62,57 +55,62 @@ class OUActionNoise:
         else:
             self.x_prev = np.zeros_like(self.mean)
 
+
 class DDPG():
     def __init__(self, env, buffer_capacity=1000, learning_rate=0.01, batch_size=32, discount=0.9):
         self.env = env
-        self.state_dim = 3 * env.N + 2 # why add 2? -> because it is vector
-        self.action_dim = 3 * env.N + 2
-        self.learning_rate = learning_rate
+        self.state_dim = 2 * env.N + 1  # why add 2? -> because it is vector
+        self.action_dim = 2 * env.N + 1
+        self.actor_learning_rate = learning_rate
         self.critic_learning_rate = 2*learning_rate
         self.gamma = discount
         self.batch_size = batch_size
         self.buffer_capacity = buffer_capacity
-        self.train_start = 10 
-        self.xi = 0.001 # for update target networks
-        self.kn_init = tf.keras.initializers.RandomUniform(minval=-0.1, maxval=0.1)
+        self.train_start = 10
+        self.xi = 0.001  # for update target networks
+        self.kn_init = tf.keras.initializers.RandomUniform(
+            minval=-0.1, maxval=0.1)
 
-        #---- Replay memory --------------------------------------------------
-        self.buffer = deque(maxlen = self.buffer_capacity)
-        
-        #---- Creat a noise process ------------------------------------------
+        # ---- Replay memory --------------------------------------------------
+        self.buffer = deque(maxlen=self.buffer_capacity)
+
+        # ---- Creat a noise process ------------------------------------------
         self.mean = 0.0
         self.std = 0.01
         self.epsilon = 1
         self.epsilon_decay = 0.9995
         self.epsilon_min = 0.01
-        self.noise = OUActionNoise(mean = self.mean*np.ones(self.action_dim), 
-                             std_deviation = self.std*np.ones(self.action_dim))
-        
-        #---- Create actor and critic ----------------------------------------
+        self.noise = OUActionNoise(mean=self.mean*np.ones(self.action_dim),
+                                   std_deviation=self.std*np.ones(self.action_dim))
+
+        # ---- Create actor and critic ----------------------------------------
         self.actor = self.get_actor()
         self.target_actor = self.get_actor()
         self.critic = self.get_critic()
         self.target_critic = self.get_critic()
-        
+
         # Make the weights equal initially
         self.target_actor.set_weights(self.actor.get_weights())
         self.target_critic.set_weights(self.critic.get_weights())
-        
-        self.actor_optimizer = tf.keras.optimizers.Adam(self.actor_learning_rate)
-        self.critic_optimizer = tf.keras.optimizers.Adam(self.critic_learning_rate)
+
+        self.actor_optimizer = tf.keras.optimizers.Adam(
+            self.actor_learning_rate)
+        self.critic_optimizer = tf.keras.optimizers.Adam(
+            self.critic_learning_rate)
 
     def record(self, obs_tuple):
         # Saves experience tuple (s,a,r,s') in the replay memory
         self.buffer.append(obs_tuple)
-    
+
     def get_actor(self):
         inputs = layers.Input(shape=(self.state_dim,))
         hidden = layers.Dense(128, activation="relu")(inputs)
         hidden = layers.Dense(128, activation="relu")(hidden)
-        
+
         # action = [ut, fnt, pnt, bnt]
-        action = layers.Dense(self.action_dim, activation="sigmoid", kernel_initializer=self.kn_init)(hidden)
-        
+        action = layers.Dense(
+            self.action_dim, activation="sigmoid", kernel_initializer=self.kn_init)(hidden)
+
         # Outputs actions
         model = keras.Model(inputs=inputs, outputs=action)
         return model
@@ -122,36 +120,36 @@ class DDPG():
         state_input = layers.Input(shape=(self.state_dim,))
         state_out = layers.Dense(128, activation="relu")(state_input)
         state_out = layers.Dense(128, activation="relu")(state_out)
-        
+
         # Action as input
         action_input = layers.Input(shape=(self.action_dim,))
         action_out = layers.Dense(128, activation="relu")(action_input)
         action_out = layers.Dense(128, activation="relu")(action_out)
-    
+
         # Both are passed through seperate layer before concatenating
         concat = layers.Concatenate()([state_out, action_out])
-        
+
         hidden = layers.Dense(256, activation="relu")(concat)
         hidden = layers.Dense(256, activation="relu")(hidden)
         Qvalue = layers.Dense(1)(hidden)
-        
+
         # Outputs Q-value for given state-action
         model = keras.Model(inputs=[state_input, action_input], outputs=Qvalue)
         return model
-   
+
     def convert_to_vector(self, var_in):
         # Convert a state into a vector for Tensorflow process
         out = np.empty(0)
         for var in var_in:
-            out = np.append(out, np.reshape(var, (1,-1)))
+            out = np.append(out, np.reshape(var, (1, -1)))
         return out
 
     def policy(self, state, scheme='Proposed'):
         # Return an action sampled from the actor DNN plus some noise for exploration
         # Convert the state into a vector, then to a tensor
         state_vector = self.convert_to_vector(state)
-        tf_state = tf.expand_dims(tf.convert_to_tensor(state_vector),0)
-        
+        tf_state = tf.expand_dims(tf.convert_to_tensor(state_vector), 0)
+
         # Sample action from the actor, and add noise to the sampled actions
         sampled_action = tf.squeeze(self.actor(tf_state))
         sampled_action = sampled_action.numpy() + self.epsilon * self.noise()
@@ -159,25 +157,29 @@ class DDPG():
             self.epsilon *= self.epsilon_decay
 
          # Make sure actions are within bounds
-        action = np.clip(sampled_action,0,1)
-        
+        action = np.clip(sampled_action, 0, 1)
+
         # actions = [ut, fnt, pnt, bnt]
 
         # will change below
-        fus= action[0]
-        fli = action[1:self.env.N + 2]
-        ois = action[self.env.N + 2:]
-        
+        fus = action[0:1]
+        fli = action[1:self.env.N+1]
+        ois = action[self.env.N+1:]
+        # print(f'in policy action shape: {np.shape(action)} \n')
+        # print(f'in  policy action {action}\n')
+        # print(f'in policy fus: {fus} \n fli: {fli} \n ois: {ois} \n')
+        # print(f'in policy \n fus shape: {np.shape(fus)} \n fli shape: {np.shape(fli)} \n ois shape: {np.shape(ois)} \n')
+
         if scheme == 'Uniform':
             fus = 0.9*np.random.rand(1)
             fli = 0.9*np.random.rand(self.env.N)
-            ois = np.random.rand(0,1,self.env.N)
-        
+            ois = np.random.rand(0, 1, self.env.N)
+
         if scheme == 'Random':
             fus = 0.9*np.random.rand(1)
-            fli = np.random.rand(1,5,self.env.N)
-            ois = np.random.rand(0,1,self.env.N)
-        
+            fli = np.random.rand(1, 5, self.env.N)
+            ois = np.random.rand(0, 1, self.env.N)
+
         return [fus, fli, ois]
 
      # Use tf.function to speed up blocks of code that contain many small TensorFlow operations.
@@ -186,33 +188,39 @@ class DDPG():
         # Train the actor and the critic
         with tf.GradientTape() as tape:
             target_actions = self.target_actor(state_next_batch, training=True)
-            y = reward_batch + self.gamma * self.target_critic([state_next_batch, target_actions], training=True)
-            critic_value = self.critic([state_batch, action_batch], training=True)
+            y = reward_batch + self.gamma * \
+                self.target_critic(
+                    [state_next_batch, target_actions], training=True)
+            critic_value = self.critic(
+                [state_batch, action_batch], training=True)
             critic_loss = tf.math.reduce_mean(tf.math.square(y - critic_value))
-        critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
-        self.critic_optimizer.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
-        
+        critic_grad = tape.gradient(
+            critic_loss, self.critic.trainable_variables)
+        self.critic_optimizer.apply_gradients(
+            zip(critic_grad, self.critic.trainable_variables))
+
         with tf.GradientTape() as tape:
             actions = self.actor(state_batch, training=True)
             critic_value = self.critic([state_batch, actions], training=True)
             # Used `-value` as we want to maximize the value given by the critic for our actions
             actor_loss = -tf.math.reduce_mean(critic_value)
         actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
-        self.actor_optimizer.apply_gradients(zip(actor_grad, self.actor.trainable_variables))
-    
+        self.actor_optimizer.apply_gradients(
+            zip(actor_grad, self.actor.trainable_variables))
+
     @tf.function
     def update_target(self):
         for (a, b) in zip(self.target_actor.variables, self.actor.variables):
             a.assign(b * self.xi + a * (1 - self.xi))
-        
+
         for (c, d) in zip(self.target_critic.variables, self.critic.variables):
             c.assign(d * self.xi + c * (1 - self.xi))
-    
+
     def update_model(self):
         # Select random samples from the buffer to train the actor and the critic
         if len(self.buffer) < self.train_start:
             return
-        
+
         indices = np.random.choice(len(self.buffer), self.batch_size)
         state_batch, action_batch, reward_batch, state_next_batch = [], [], [], []
         for i in indices:
@@ -220,13 +228,13 @@ class DDPG():
             action_batch.append(self.convert_to_vector(self.buffer[i][1]))
             reward_batch.append(self.buffer[i][2])
             state_next_batch.append(self.convert_to_vector(self.buffer[i][3]))
-        
+
         # Convert to tensors
         state_batch = tf.convert_to_tensor(state_batch)
         action_batch = tf.convert_to_tensor(action_batch)
         reward_batch = tf.convert_to_tensor(reward_batch)
         reward_batch = tf.cast(reward_batch, dtype=tf.float32)
         state_next_batch = tf.convert_to_tensor(state_next_batch)
-        
+
         # Update parameters
         self.update(state_batch, action_batch, reward_batch, state_next_batch)
--- environment.py
+++ environment.py
@@ -1,9 +1,19 @@
-"""
-This file is system environment for agent.py.
-"""
-
+import numpy as np
+import os
+import tensorflow as tf
+from itertools import cycle
+from tensorflow import keras
+from collections import deque
+from tensorflow.keras import layers
 import numpy as np
 
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+
+# ------------------------------------environment----------------------------
 class UAV():
     '''
     <Constants AND Variables>
@@ -16,7 +26,7 @@ class UAV():
         Pf: flying power of the UAV (watts)
         Ph: hovering power of the UAV (watts)
         v: UAV velocity (m/s)
-        
+
         iota: chip energy consumption coefficient (kappa also same)
         FU: number of UAV CPU cycle per bit (bits)
         FI: number of GUs CPU cycle per bit (bits)
@@ -28,7 +38,7 @@ class UAV():
         beta: channel power gain at reference distance d0 = 1 m (dB)
         alpha: path-loss exponent
         sigma: noise power spectral density (dBm/Hz)
-        
+
         Emax: battery capacity of the user, (mJ)
         mu: average harvested energy (mJ)
 
@@ -44,47 +54,47 @@ class UAV():
         O: range of offlaoding ratio 
 
     '''
-    def __init__(self, H=20, N=10, T=100, ts=6, Eb=8, v=15, Pf=2, Ph=1, iota=1e-27, FI=[1,5], FU=[20,30], D=[0.4,1], B=30, 
-                 beta=10, alpha=2, sigma=-174, Emax=10, eta=0.8, ppb = 4, pis=[1.0,10.0], w = 0.1, O =[0,1]):
-         self.H = H
-         self.N = N
-         self.T = T
-         self.ts = ts
-         self.Eb = Eb
-         self.Pf = Pf
-         self.Ph = Ph
-         self.v = v
-        
-         self.iota = iota
-         self.FI = np.array(FI) * 1e9
-         self.FU = np.array(FU) * 1e9
-         self.D = np.array(D)* (10**6)
-        
-         self.B = B*1e6
-        #  self.pn = np.array(pn) *1e-3
-         self.beta = beta**(-30/10)
-         self.alpha = alpha
-         self.sigma = 10**(sigma/10)*1e-3
-        
-         self.Es = [0.1,Emax]
-         
-         self.alpha = 0.1
-        
-         self.eta = eta
-         self.ppb = ppb
-         self.tau = self.T*0.9
-         self.pis = np.random.uniform(pis[0],pis[1],self.N) * 1e-3
-         
-         self.wis = np.random.rand(self.N, 2)   
-         self.wo = np.array([0,0])  
-         self.wb = np.array([9,9])
-        
-         self.w = w
-         self.fli = np.random.uniform(0.5,1,self.N)
-         self.O = np.array(O)
-         
-         self.Eh = self.rf_eh()
-                          
+
+    def __init__(self, H=20, N=10, T=40, ts=6, Eb=8, v=15, Pf=2, Ph=1, iota=1e-27, FI=[1, 5], FU=[20, 30], D=[0.4, 1], B=30,
+                 beta=30, alpha=2, sigma=-174, Emax=10, eta=1e-28, ppb=4, pis=[1.0, 10.0], w=0.1, O=[0, 1]):
+        self.H = H
+        self.N = N
+        self.T = T
+        self.ts = ts
+        self.Eb = Eb
+        self.Pf = Pf
+        self.Ph = Ph
+        self.v = v
+
+        self.iota = iota
+        self.FI = np.array(FI)*1e5
+        self.FU = np.array(FU)*1e5
+        self.D = np.array(D) * (10**6)
+
+        self.B = B*1e6
+       #  self.pn = np.array(pn) *1e-3
+        self.beta = beta**(-30/10)
+        self.alpha = alpha
+        self.sigma = 10**(sigma/10)*1e-3
+
+        self.Es = [0.1, Emax]
+
+        self.alpha = 0.1
+
+        self.eta = eta
+        self.ppb = ppb
+        self.tau = self.T*0.9
+        self.pis = np.random.uniform(pis[0], pis[1], self.N) * 1e-3
+
+        self.wis = np.random.rand(self.N, 2)
+        self.wo = np.array([0, 0])
+        self.wb = np.array([9, 9])
+
+        self.w = w
+        self.O = np.array(O)
+
+        self.Eh = self.rf_eh()
+
     '''
     wo: UAV locate
     wb: power beacon locate
@@ -92,14 +102,18 @@ class UAV():
     dis: inputdata of user on time slot s (uniform dist.)
     eus: UAV energy consumption on time slot s, will change (uniform dist.)
     '''
-    def reset(self):
-        ois_1 = np.random.uniform(self.O[0],self.O[1],self.N) 
-        Dis = np.random.uniform(self.D[0],self.D[1],self.N) # change the size 
-        Eus = np.random.rand(self.N) # also
-        state = [ois_1, Dis, Eus] # STATE
 
-        return state 
-    
+    def reset(self):
+        ois_1 = np.random.uniform(self.O[0], self.O[1], self.N)
+        # print(f'in reset ois_1: {ois_1} \n')
+        Dis = np.random.uniform(
+            self.D[0], self.D[1], self.N)  # change the size
+        # print(f'in reset Dis{Dis} \n ')
+        Eus = np.random.rand()
+        state = [ois_1, Dis, Eus]  # STATE
+
+        return state
+
     '''
     dis: distance between UAV and GU 
     gis: channel gain between UAV and GU
@@ -111,16 +125,19 @@ class UAV():
     Et: energy consumption for transmission
     ttis: local execution time at GU in time slot
     '''
+
     def local(self, fli, ois_1, Dis):
-        Fi = np.random.uniform(self.FU[0],self.FU[1],self.N)
+        Fi = np.random.uniform(self.FI[0], self.FI[1], self.N)
         dis = np.sqrt(self.H**2 + np.sum((self.wo-self.wis)**2))
         gis = self.beta / (dis**self.alpha)
-        ris = self.B * np.log2(1+ ((self.pis*gis)/self.sigma**2))
-        
+        ris = self.B * np.log2(1 + ((self.pis*gis)/self.sigma**2))
+
+        # print(f'in local \n fli shape:{np.shape(fli)} \n ois_1 shape:{np.shape(ois_1)} \n Dis shape:{np.shape(Dis)} \n')
+
         Elc = self.iota * (fli**2) * Fi * (1-ois_1)*Dis
         tlc = ((1-ois_1)*Dis) / fli
-        
-        Et = (self.pis * (1-ois_1)*Dis) /ris
+
+        Et = (self.pis * (1-ois_1)*Dis) / ris
         ttis = ((1-ois_1)*Dis)/ris
 
         Elis = Elc + Et
@@ -131,20 +148,26 @@ class UAV():
     Eexe: energy consumption at the UAV for processing the data 
     texe: execution time at the UAV for processing the offloaded data
     '''
+
     def computing(self, fus, ois_1, Dis):
-        F = np.random.uniform(self.FU[0], self.FU[1], self.N)
-        Eexe = self.iota*((fus)**2)*self.FU*(ois_1*Dis) 
+        FU = np.random.randint(self.FU[0], self.FU[1], dtype='int64')
+
+        # print(f'in computing \n fus shape: {np.shape(fus)} \n ois_1 shape: {np.shape(ois_1)} \n Dis shape: {np.shape(Dis)}, \n FU: {np.shape(self.FU)} \n')
+        # print(f'FU rand int {self.FU[0], self.FU[1]} \n')
+
+        Eexe = self.iota*((fus)**2)*FU*(ois_1*Dis)
         texe = (ois_1 * Dis) / fus
         return Eexe, texe
-        # Eexe = self.iota*((fus)^2)*self.FU 
+        # Eexe = self.iota*((fus)^2)*self.FU
         # texe = (ois_1 * Dis) / fus
         # return Eexe, texe
-    
+
     '''
     db2u: distace between the UAV and power beacon
     gpb: channel gain between the UAV and power beacon
     Eh: harvested energy  
     '''
+
     def rf_eh(self):
         db2u = np.sqrt(self.H**2 + np.sum((self.wo - self.wb)**2))
         gpb = self.beta / (db2u**self.alpha)
@@ -155,11 +178,12 @@ class UAV():
     State = [ois_1, dis, eus]
     Action = [pis, fus]
     '''
+
     def step(self, state, action):
         # will check this env.
         # extract information from state
-        ois_1 = state[0] # first state is randomly
-        Dis = state[1]
+        ois_1 = state[0]  # first state is randomly
+        Dis = state[1] * (self.D[1]-self.D[0]) + self.D[0]
         Eus = state[2]
 
         # extract information from state
@@ -168,27 +192,34 @@ class UAV():
         fli = action[1] * (self.FI[1]-self.FI[0]) + self.FI[0]
         ois = action[2]
 
-
-        # need adjust Fi  
+        # need adjust Fi
         # Fi = self.FU
         # fli = self.
 
         Elis, tlis = self.local(fli, ois_1, Dis)
         Eexe, texe = self.computing(fus, ois_1, Dis)
 
-        # --------------- reward ------------------  
+        # --------------- reward ------------------
         alarm = (tlis < self.ts) | (texe < self.ts)
-        tlis[alarm], texe[alarm] = tlis, texe 
-        reward = np.sum(Elis + Eexe) + self.w*abs(tlis-self.ts) + self.w * abs(texe-self.ts)
-        # ---------------  update state --------------- 
-        ois_nxt = ois  
-        Dis_nxt = np.random.rand(self.D[0], self.D[1], self.N)
+        # print(f'in step alarm {alarm}')
+        tlis, texe = tlis[alarm], texe[alarm]
+
+        # print(f'in step raward sum(Elis+Eexe) {np.sum(Elis + Eexe)} \n w*tlis {self.w * abs(tlis-self.ts)} \n w*texe {self.w * abs(texe-self.ts)} \n')
+        # print(f'in step shapes about \n Elis {np.shape(Elis)} \n Eexe {np.shape(Eexe)} \n  tlis {np.shape(tlis)}  \ texe {np.shape(texe)}')
+
+        reward = np.sum(Elis + (Eexe)) + self.w * np.sum(abs(tlis -
+                                                             self.ts)) + self.w * np.sum(abs(texe-self.ts))
+
+        # print(f'in step reward {reward} \n')
+        # ---------------  update state ---------------
+        ois_nxt = ois
+        Dis_nxt = np.random.uniform(self.D[0], self.D[1], self.N)
+        # print(f'in step Dis_nxt: {Dis_nxt} \n')
         Eus_nxt = Eus - np.sum(Eexe) + self.Eh
-        # Eh 
+        Eus_nxt = np.clip(Eus_nxt, self.Es[0], self.Es[1])
+        Eus_nxt = (Eus_nxt - self.Es[0]) / (self.Es[1]-self.Es[0])
+        # Eh
 
         next_state = [ois_nxt, Dis_nxt, Eus_nxt]
         # update state
         return reward, next_state
-
-#%% Test
-# uav_env = UAV()
